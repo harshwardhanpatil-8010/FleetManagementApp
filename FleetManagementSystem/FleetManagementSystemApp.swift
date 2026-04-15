2c25b449c47c7360ec324bc8ec9a492dc519cea5
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2026-04-15 09:54:13 +0530

    new

---
+++ FleetManagementSystem/FleetManagementSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  FleetManagementSystemApp.swift
+//  FleetManagementSystem
+//
+//  Created by harshwardhan patil on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FleetManagementSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
